f4c7108e955d4dd3cf09220529ea7a611ebf1e05
Code tansarian <codetansarian@gmail.com>
2026-05-14 02:26:22 +0330

    textbox.py

---
+++ textbox.py
@@ -0,0 +1,25 @@
+from rubkey import Rubkey, Keypad, TextboxButton
+
+TOKEN = "توکن_واقعی_شما"
+
+bot = Rubkey(token=TOKEN)
+
+@bot.command("start")
+def start(msg):
+    msg.reply("سلام. از دستور /get استفاده كنيد.")
+
+@bot.command("get")
+def ask_text(msg):
+    keypad = Keypad(one_time=True)
+    keypad.add_row(
+        TextboxButton("name_btn", "ورود متن", 
+                      place_holder="متن خود را بنويسيد...",
+                      title="ورود اطلاعات")
+    )
+    msg.reply("لطفا متن خود را وارد كنيد:", keypad=keypad)
+
+@bot.on_button("name_btn")
+def get_text(msg):
+    msg.reply("متن شما: " + msg.text)
+
+bot.run()
